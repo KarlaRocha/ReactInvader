9c9142c8e64979019c101524852a1be8117884dc
Karla Rocha <karlarochanr@gmail.com>
2019-08-28 21:45:34 -0500

    Added hooks + basic ship movement

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,78 @@
-import React, {Component} from "react" 
+import React, { useState, useEffect } from "react" 
 import ReactDOM from "react-dom" 
 
-class App extends Component {
-    render() {
-        return <h1>Hello React Invader!</h1> 
+
+function App() {
+    const div = React.createRef()
+    const step = 16
+
+    const [top, setTop] = useState(100)
+    const [left, setLeft] = useState(100)
+
+    function handleKeyDown(event: any){
+        // console.log('hi');
+        
+        // console.log(event.key);
+        // console.log(event.keyCode);
+        // console.log(event.code);    
+        // console.log(event.which);
+        
+        if(event.key === 'ArrowDown'){
+            console.log(div.current.offsetLeft, div.current.offsetTop);
+        }
+        switch (event.key) {
+            case 'ArrowDown':
+                setTop(top + step)
+                break;
+            case 'ArrowUp':
+                setTop(top - step)
+                break;
+            case 'ArrowLeft':
+                setLeft(left - step)
+                break;
+            case 'ArrowRight':
+                setLeft(left + step)
+                break;
+            default:
+                break;
+        }
     }
+
+    useEffect(
+        () => window.document.addEventListener("keyDown", handleKeyDown, false), 
+        []
+    )
+
+    useEffect(() => {
+        return () => {
+            window.document.removeEventListener("keyDown", handleKeyDown, false)
+        }
+    }, [])
+
+    return(
+        <>
+            <h1>Hello React Invader!</h1>
+            <div 
+                style={{ 
+                    position: "absolute", 
+                    width: "700px", 
+                    height: "500px"
+                }}
+                className="room" 
+                onKeyDown={handleKeyDown}
+                tabIndex={0}>
+                    <div 
+                        style={{
+                            position: "absolute",
+                            top: top,
+                            left: left
+                        }}
+                        ref={div}>
+                            ship
+                    </div>
+            </div>
+        </> 
+    ) 
 }
 
 ReactDOM.render(
